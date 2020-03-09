74f9c22b2a5957c1d954b82d82fdc16f6aadc78f
mbadziong <traceur775@gmail.com>
2020-03-09 06:35:02 +0100

    dump

--- src/main/scala/pl/mbadziong/Drone.scala
+++ src/main/scala/pl/mbadziong/Drone.scala
@@ -10,22 +10,71 @@ import scala.concurrent.duration._
 
 object Drone {
 
+//  def apply(droneId: Long, operator: String, tick: FiniteDuration = 1.millis): Behavior[Command] =
+//    Behaviors.setup { context =>
+//      Behaviors.withTimers { timers =>
+//        new Drone(context, timers, droneId, operator, tick)
+//      }
+//    }
+
   def apply(droneId: Long, operator: String, tick: FiniteDuration = 1.millis): Behavior[Command] =
-    Behaviors.setup { context =>
-      Behaviors.withTimers { timers =>
-        new Drone(context, timers, droneId, operator, tick)
-      }
+    Behaviors.withTimers { timers =>
+      docked(timers, droneId, operator, Position(0, 0), tick)
     }
 
   sealed trait Command
-  final case object BootDrone                                                                       extends Command
-  final case object TurnOffDrone                                                                    extends Command
-  final case class Fly(flightRequest: FlightRequest, sender: ActorRef[FlightResponse])              extends Command
-  final case class DuringFlight(flightRequest: FlightRequest, sender: ActorRef[FlightResponse])     extends Command
-  final case class SetPosition(requestId: Long, position: Position, replyTo: ActorRef[PositionSet]) extends Command
-  final case class ReadState(requestId: Long, replyTo: ActorRef[RespondState])                      extends Command
+  final case object BootDrone                                                                           extends Command
+  final case object TurnOffDrone                                                                        extends Command
+  final case class Fly(flightRequest: FlightRequest, sender: ActorRef[FlightResponse])                  extends Command
+  private final case class DuringFlight(flightRequest: FlightRequest, sender: ActorRef[FlightResponse]) extends Command
+  final case class SetPosition(requestId: Long, position: Position, replyTo: ActorRef[PositionSet])     extends Command
+  final case class ReadState(requestId: Long, replyTo: ActorRef[RespondState])                          extends Command
   final case class RespondState(requestId: Long, droneId: Long, position: Option[Position])
   final case class PositionSet(requestId: Long)
+
+  def docked(timers: TimerScheduler[Command], id: Long, operator: String, position: Position, tick: FiniteDuration): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case BootDrone =>
+          context.log.info(s"Drone [$operator | $id] booted")
+          Behaviors.same
+        case Fly(flyRequest: FlightRequest, replyTo: ActorRef[FlightResponse]) =>
+          context.log.info(s"Drone [$operator | $id] accepted $flyRequest")
+          context.self ! DuringFlight(flyRequest, replyTo)
+          Behaviors.same
+        case TurnOffDrone =>
+          context.log.info(s"Drone [$operator | $id] has been turned off")
+          Behaviors.stopped
+        case ReadState(requestId, replyTo) =>
+          context.log.info(s"State for drone $id is $position")
+          replyTo ! RespondState(requestId, id, position)
+          Behaviors.same
+      }
+    }
+
+  def flying(timers: TimerScheduler[Command], id: Long, operator: String, position: Position, tick: FiniteDuration): Behavior[Command] =
+    Behaviors.receive { (context, message) =>
+      message match {
+        case DuringFlight(flightRequest: FlightRequest, replyTo: ActorRef[FlightResponse]) =>
+          flightRequest.route match {
+            case ::(head, tail) =>
+              context.log.info(s"Drone [$operator | $id] is during flight at position $head")
+              timers.startSingleTimer(DuringFlight(new FlightRequest(flightRequest.id, tail), replyTo), tick)
+              flying(timers, id, operator, head, tick)
+            case Nil =>
+              context.log.info(s"Drone [$operator | $id] has ended flight id ${flightRequest.id}")
+              replyTo ! new FlightResponse(flightRequest.id)
+              docked(timers, id, operator, position, tick)
+            case TurnOffDrone =>
+              context.log.info(s"Drone [$operator | $id] has been turned off")
+              Behaviors.stopped
+            case ReadState(requestId, replyTo) =>
+              context.log.info(s"State for drone $id is $position")
+              replyTo ! RespondState(requestId, id, position)
+              this
+          }
+      }
+    }
 }
 
 class Drone(context: ActorContext[Drone.Command], timers: TimerScheduler[Command], val id: Long, val operator: String, tick: FiniteDuration)
@@ -53,21 +102,6 @@ class Drone(context: ActorContext[Drone.Command], timers: TimerScheduler[Command
         this.position = Some(position)
         context.log.info(s"Drone [$operator | $id] set position to ${position.lat} and ${position.lon}, requestId: $requestId")
         replyTo ! PositionSet(requestId)
-        this
-      case Fly(flyRequest: FlightRequest, replyTo: ActorRef[FlightResponse]) =>
-        context.log.info(s"Drone [$operator | $id] accepted $flyRequest")
-        context.self ! DuringFlight(flyRequest, replyTo)
-        this
-      case DuringFlight(flightRequest: FlightRequest, replyTo: ActorRef[FlightResponse]) =>
-        flightRequest.route match {
-          case ::(head, tail) =>
-            context.log.info(s"Drone [$operator | $id] is during flight at position $head")
-            position = Some(head)
-            timers.startSingleTimer(DuringFlight(new FlightRequest(flightRequest.id, tail), replyTo), tick)
-          case Nil =>
-            context.log.info(s"Drone [$operator | $id] has ended flight id ${flightRequest.id}")
-            replyTo ! new FlightResponse(flightRequest.id)
-        }
         this
     }
   }
